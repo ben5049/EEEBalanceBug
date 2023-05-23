0fd7c47ebf0a42182dac0ba203b4764089707924
Ben Smith <bens15351@gmail.com>
2023-05-23 02:07:51 +0100

    Finished logic to find white areas

--- FPGA/W_DETECT/ip/EEE_IMGPROC/px_classify.sv
+++ FPGA/W_DETECT/ip/EEE_IMGPROC/px_classify.sv
@@ -1,10 +1,10 @@
 module px_classify(
-	input logic [7:0] red, green, blue;
+	input logic [7:0] red, green, blue,
 	output logic white);
 	
 	parameter white_threshold= 8'd230;
 	always_comb
 		if (red >= white_threshold & green >= white_threshold & blue >= white_threshold) white = 1;
-		else assign white=0;
+		else white = 0;
 		
 endmodule
