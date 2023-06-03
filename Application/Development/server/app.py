d3c396998b574e73f2acade771a2406f6fe15d86
aranyagupta <ag1421@ic.ac.uk>
2023-06-03 15:23:01 +0100

    api bug fixes

--- Application/Development/server/app.py
+++ Application/Development/server/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request, make_response
 import tremaux, dijkstra
 import mariadb
-# TODO: database stuff
+# TODO: error handling
 
 app = Flask(__name__)
 
-hostip = '3.86.226.249'
+hostip = '3.88.27.3'
 
 try:
     conn = mariadb.connect(
@@ -26,7 +26,7 @@ rovers = []
 def hello():
     return jsonify({"hello":"world"})
 
-# rover communication
+# rover communication - works
 @app.route("/rover", methods=["POST"])
 def rover():
     data = request.get_json() # data has keys "diagnostics", "MAC", "nickname", ""timestamp", "position", "whereat", "orientation", "branches", "beaconangles", "tofleft", "tofright"
@@ -43,19 +43,34 @@ def rover():
         cur.execute("INSERT INTO Rovers (MAC, nickname) VALUES (? , ?)", (data["MAC"], data["nickname"]))
         cur.execute("INSERT INTO Sessions (MAC,  SessionNickname) VALUES (?, ?)", (data["MAC"], data["timestamp"]))
         cur.execute("SELECT MAX(SessionID) FROM Sessions WHERE MAC=?", (data["MAC"],))
-        r.sessionId = cur[0]
+        for x in cur:
+            r.sessionId = x[0]
         
         rovers.append(r)
 
     resp = r.tremaux(data["position"], data["whereat"], data["branches"], data["beaconangles"])
     resp = {"next_actions" : resp}
-
+    print(data)
     # store positions and timestamp in database
-    cur.execute("INSERT INTO Diagnostics (MAC, timestamp, battery, CPU, connection) VALUES (?, ?, ?, ?, ?)", (data["MAC"], data["timestamp"], data["diagnostics"]["battery"], data["diagnostics"]["CPU"], data["diagnostics"]["connection"]))
     cur.execute("INSERT INTO ReplayInfo (timestamp, xpos, ypos, whereat, orientation, tofleft, tofright, MAC, SessionID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (data["timestamp"], data["position"][0], data["position"][1], data["whereat"], data["orientation"], data["tofleft"], data["tofright"], data["MAC"], r.sessionId))
+    cur.execute("INSERT INTO Diagnostics (MAC, timestamp, battery, CPU, connection) VALUES (?, ?, ?, ?, ?)", (data["MAC"], data["timestamp"], data["diagnostics"]["battery"], data["diagnostics"]["CPU"], data["diagnostics"]["connection"]))
+
+    # cur.execute("SELECT * FROM Rovers")
+    # for mac, nickname in cur:
+    #     print(mac, nickname)
+    # cur.execute("SELECT * FROM Diagnostics")
+    # for mac, timestamp, battery, cpu, connection in cur:
+    #     print(mac, timestamp, battery, cpu, connection)
+    cur.execute("SELECT * FROM ReplayInfo")
+    for timestamp, xpos, ypos, whereat, orientation, tofleft, tofright, mac, SessionID in cur:
+        print(timestamp, xpos, ypos, whereat, orientation, tofleft, tofright, mac, SessionID)
+    # cur.execute("SELECT * FROM Sessions")
+    # for mac, sessionId, SessionNickname in cur:
+    #     print(mac, sessionId, sessionNickname)
 
     return make_response(jsonify(resp), 200)
 
+# works
 @app.route("/client/allrovers", methods=["GET"])
 def allrovers():
     d = []
@@ -66,7 +81,7 @@ def allrovers():
         disallowedMacs.append(rover.name)
         temp["nickname"] = rover.nickname
         temp["connected"] = True
-        temp["sessionid"] = rover.sessionid
+        temp["sessionid"] = rover.sessionId
         d.append(temp)
     # get the rest of unconnected rovers from database
     t = ""
@@ -84,37 +99,47 @@ def allrovers():
         d.append(temp)
     return make_response(jsonify(d), 200)
 
-
+# works
 @app.route("/client/replay", methods=["GET"])
 def replay():
     data = request.get_json()
-    sessionid = data["sessionid"]
-    info = cur.execute("SELECT * FROM ReplayInfo WHERE SessionID=?", (sessionid,))
+    sessionid = int(data["sessionid"])
+    cur.execute("SELECT * FROM ReplayInfo")
+    for timestamp, xpos, ypos, whereat, orientation, tofleft, tofright, mac, SessionID in cur:
+        print(timestamp, xpos, ypos, whereat, orientation, tofleft, tofright, mac, SessionID)
+    print(sessionid)
+    cur.execute("SELECT * FROM ReplayInfo WHERE SessionID=?", (sessionid,))
+    if cur is None:
+        return make_response(jsonify({"error":"Session does not exist"}), 404)
     resp = {}
-    for timestamp, xpos, ypos, whereat, orientation, tofleft, tofright, mac, sessionid in info:
+    for timestamp, xpos, ypos, whereat, orientation, tofleft, tofright, mac, sessionid in cur:
         resp[timestamp] = [xpos, ypos, whereat, orientation, tofleft, tofright]
     return make_response(jsonify(resp))
 
+# works
 @app.route("/client/sessions", methods=["GET"])
 def sessions():
-    cur.execute("SELECT * FROM SESSIONS ORDER BY SessionId DESC;")
+    cur.execute("SELECT * FROM Sessions ORDER BY SessionId DESC;")
     d = {}
     for mac, sessionid, nickname in cur:
         d[sessionid] = [mac, nickname]
     return make_response(jsonify(d), 200)
 
+# works
 @app.route("/client/diagnostics", methods=["GET"])
 def diagnostics():
     data = request.get_json()
-    instr = "SELECT * FROM Diagnostics WHERE MAC=? ORDER BY timestamp DESC LIMIT 1;"
-    cur.execute(instr, (data["MAC"]))
-    d = {"MAC":cur[0], "timestamp":cur[1], "battery":cur[2], "CPU":cur[3], "connection":cur[4]}
-    return make_response(jsonify(d))
+    cur.execute("SELECT * FROM Diagnostics WHERE MAC=? ORDER BY timestamp DESC LIMIT 1;", (data["MAC"],))
+    d = {}
+    for mac, timestamp, battery, cpu, connection in cur:
+        d = {"MAC":mac, "timestamp":timestamp, "battery":battery, "CPU":cpu, "connection":connection}
+    print(d)
+    return make_response(jsonify(d), 200)
 
 @app.route("/client/visualisations", methods=["GET"])
 def visualisations():
     return
-
+# works
 @app.route("/client/pause", methods=["POST"])
 def pause():
     data = request.get_json()
@@ -125,6 +150,7 @@ def pause():
 
     return make_response(jsonify({"success":"successfully paused rover"}), 200)
 
+# works
 @app.route("/client/play", methods=["POST"])
 def play():
     data = request.get_json()
@@ -134,14 +160,16 @@ def play():
             rover.pause = False
     return make_response(jsonify({"success":"successfully played rover"}), 200)
 
+# works
 @app.route("/client/setsessionnickname", methods=["POST"])
 def sessionNickname():
     data = request.get_json()
     session = data["sessionid"]
     nick = data["sessionNick"]
-    cur.execute("UPDATE Sessions SET SessionNickname="+nick+" WHERE SessionId=?", (session,))
+    cur.execute("UPDATE Sessions SET SessionNickname=? WHERE SessionId=?", (nick, session,))
     return make_response(jsonify({"success":"successfully updated session nickname"}), 200)
 
+# works
 @app.route("/client/addnickname", methods=["POST"])
 def addnickname():
     data = request.get_json()
@@ -151,7 +179,7 @@ def addnickname():
         if rover.name == mac:
             rover.nickname = nick
 
-    cur.execute("UPDATE Rovers SET nickname="+nick+" WHERE MAC=?", (mac,))
+    cur.execute("UPDATE Rovers SET nickname=? WHERE MAC=?", (nick, mac,))
     return make_response(jsonify({"success":"successfully changed rover/session nickname"}), 200)
 
 @app.route("/client/shortestpath", methods=["POST"])
