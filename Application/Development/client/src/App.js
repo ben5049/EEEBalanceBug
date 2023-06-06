5091be0b7a7c1d61617e32ae6287f84267980741
AdvikChitre <advik.chitre43@gmail.com>
2023-06-06 15:02:01 +0100

    Comments for Replay page

--- Application/Development/client/src/App.js
+++ Application/Development/client/src/App.js
@@ -13,13 +13,14 @@ import Replay from './pages/Replay';
 import Connected from './pages/Connected';
 
 //-------------------------------- Main -------------------------------------------------
+
 /*
 Top Level File - Contains intra-page navigation system
 */
 
 function App() {
 	/* Set IPv4 for AWS then rebuild (AWS Learner account only active for 4h) */
-	localStorage.setItem('ServerIP', '18.207.197.24');
+	localStorage.setItem('ServerIP', '44.201.77.138');
 
 	/* Connect pages to URL endpoints */
 	return (
