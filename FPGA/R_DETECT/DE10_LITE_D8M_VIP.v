34c113d64edd237ccb5ee0586020e2da6f65da5f
Ben Smith <bens15351@gmail.com>
2023-06-08 18:22:25 +0100

    Changed I2C pins

--- FPGA/R_DETECT/DE10_LITE_D8M_VIP.v
+++ FPGA/R_DETECT/DE10_LITE_D8M_VIP.v
@@ -178,11 +178,11 @@ Qsys u0 (
 		.altpll_0_phasedone_conduit_export         (),         					//       altpll_0_phasedone_conduit.export		
 		
 		.eee_imgproc_0_conduit_mode_new_signal     (SW[0]),
-		.eee_imgproc_0_conduit_i2c_scl		 		(ARDUINO_IO[12]),
-		.eee_imgproc_0_conduit_i2c_sda		 		(ARDUINO_IO[13]),
+		.eee_imgproc_0_conduit_i2c_scl		 		(ARDUINO_IO[15]),
+		.eee_imgproc_0_conduit_i2c_sda		 		(ARDUINO_IO[14]),
 		
-		.uart_0_rx_tx_rxd                          (ARDUINO_IO[1]),                          //                     uart_0_rx_tx.rxd
-		.uart_0_rx_tx_txd                          (ARDUINO_IO[0])                           //
+		.uart_0_rx_tx_rxd                          (ARDUINO_IO[3]),                          //                     uart_0_rx_tx.rxd
+		.uart_0_rx_tx_txd                          (ARDUINO_IO[2])                           //
 	);
 	
 
