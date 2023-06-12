214b9f1a17842b9ffa419846a80a608aa2ff8ef6
aranyagupta <ag1421@ic.ac.uk>
2023-06-12 16:01:53 +0100

    fix server startup

--- Application/Development/servstart.sh
+++ Application/Development/servstart.sh
@@ -3,11 +3,11 @@ DEBUG=false
 sudo git pull https://aranyagupta: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@github.com/ben5049/EEEBalanceBug
 cd client
 npm run build
-sudo cp -f build /var/www/html
-sudo cp -f public /var/www/html
-sudo cp -f src /var/www/html
-sudo cp -f package-lock.json /var/www/html
-sudo cp -f package.json /var/www/html
+sudo cp -rf build /var/www/html
+sudo cp -rf public /var/www/html
+sudo cp -rf src /var/www/html
+sudo cp -rf package-lock.json /var/www/html
+sudo cp -rf package.json /var/www/html
 cd ..
 sudo systemctl daemon-reload
 sudo systemctl start flask
