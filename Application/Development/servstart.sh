756c07cd99dd5043d5af70eace022d8aefb5a171
aranyagupta <ag1421@ic.ac.uk>
2023-06-12 15:47:23 +0100

    update server ip update sercer setup script

--- Application/Development/servstart.sh
+++ Application/Development/servstart.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 DEBUG=false 
 sudo git pull https://aranyagupta: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@github.com/ben5049/EEEBalanceBug
+cd client
+npm run build
+mv -rf build /var/www/html
+mv -rf public /var/www/html
+mv -rf src /var/www/html
+mv -rf package-lock.json /var/www/html
+mv -rf package.json /var/www/html
 sudo systemctl daemon-reload
 sudo systemctl start flask
 sudo systemctl enable flask
