fb51205d9b830cdb52c355b7546f131b47f683de
aranyagupta <ag1421@ic.ac.uk>
2023-06-12 10:59:08 +0100

    automate server updates

--- Application/Development/server/servstart.sh
+++ Application/Development/server/servstart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DEBUG=false 
-
+sudo git pull https://aranyagupta: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@github.com/ben5049/EEEBalanceBug
 sudo systemctl daemon-reload
 sudo systemctl start flask
 sudo systemctl enable flask
