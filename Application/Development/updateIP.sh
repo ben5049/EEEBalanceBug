6d83424f90021b39063fe0644e83940a02fed84b
aranyagupta <ag1421@ic.ac.uk>
2023-06-12 17:43:35 +0100

    update automatic ip update to include microcontroller

--- Application/Development/updateIP.sh
+++ Application/Development/updateIP.sh
@@ -4,7 +4,10 @@ SEDTEXT2="/ ./server/app.py"
 sudo sed -i ${SEDTEXT}${SERVER}${SEDTEXT2}
 SEDTEXT3="/ ./client/src/App.js"
 sudo sed -i ${SEDTEXT}${SERVER}${SEDTEXT3}
+SEDTEXT4="/ ../../Microcontroller\ code/main/TaskServerCommunication.cpp"
+sudo sed -i ${SEDTEXT}${SERVER}${SEDTEXT4}
 git add ./server/app.py
 git add ./client/src/App.js
+git add ../../Microcontroller\ code/main/TaskServerCommunication.cpp
 git commit -m "IP Update"
 git push
