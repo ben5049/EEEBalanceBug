87bf3e53bf4ccec4baf960090cfc7df2739074b3
aranyagupta <ag1421@ic.ac.uk>
2023-06-12 17:13:54 +0100

    add automatic ip updater

---
+++ Application/Development/updateIP.sh
@@ -0,0 +1,10 @@
+SERVER="18.234.100.102"
+SEDTEXT="s/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/"
+SEDTEXT2="/ ./server/app.py"
+sudo sed -i ${SEDTEXT}${SERVER}${SEDTEXT2}
+SEDTEXT3="/ ./client/src/App.js"
+sudo sed -i ${SEDTEXT}${SERVER}${SEDTEXT3}
+git add ./server/app.py
+git add ./client/src/App.js
+git commit -m "IP Update"
+git push
