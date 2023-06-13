bd5a7de3fbb4c863e01485b5a0e19e4f09583f40
aranyagupta <ag1421@ic.ac.uk>
2023-06-13 14:49:36 +0100

    commented ip changing scripts

--- Application/Development/servstart.sh
+++ Application/Development/servstart.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 DEBUG=false
+# ask user for server ip: "1.1.1.1"
 SERVER=$1
+# regex describing ip address format
 SEDTEXT="s/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/"
+# regex describing file to change
 SEDTEXT2="/ /etc/nginx/conf.d/react.conf"
+# pull any changes and update nginx service
 sudo git pull https://aranyagupta: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@github.com/ben5049/EEEBalanceBug
 sudo sed -i ${SEDTEXT}${SERVER}${SEDTEXT2}
+# build with new ip address and copy build to necessary place
 cd client
 npm run build
 sudo cp -rf /home/ubuntu/EEEBalanceBug/Application/Development/client/build/* /var/www/html/react
 cd ..
+# reload the daemon and enable flask + nginx
 sudo systemctl daemon-reload
 sudo systemctl start flask
 sudo systemctl enable flask
--- Application/Development/updateIP.sh
+++ Application/Development/updateIP.sh
@@ -1,11 +1,15 @@
+# ask user for server ip
 SERVER=$1
+# regex for ip address
 SEDTEXT="s/[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*/"
+# change server ip for database usage and client & rover API requests  
 SEDTEXT2="/ ./server/app.py"
 sudo sed -i ${SEDTEXT}${SERVER}${SEDTEXT2}
 SEDTEXT3="/ ./client/src/App.js"
 sudo sed -i ${SEDTEXT}${SERVER}${SEDTEXT3}
 SEDTEXT4="/ ../../Microcontroller_code/main/TaskServerCommunication.cpp"
 sudo sed -i ${SEDTEXT}${SERVER}${SEDTEXT4}
+# push changed files to server
 git add ./server/app.py
 git add ./client/src/App.js
 git add ../../Microcontroller_code/main/TaskServerCommunication.cpp
