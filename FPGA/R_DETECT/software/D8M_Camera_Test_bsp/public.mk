d6021844f3d2ab7ef72060d87c6b54ba11db5fcc
Ben Smith <bens15351@gmail.com>
2023-06-15 12:50:29 +0100

    hex

--- FPGA/R_DETECT/software/D8M_Camera_Test_bsp/public.mk
+++ FPGA/R_DETECT/software/D8M_Camera_Test_bsp/public.mk
@@ -157,9 +157,9 @@ SOPC_SYSID_FLAG += --sidp=0x410e0
 ELF_PATCH_FLAG  += --sidp 0x410e0
 
 # The SOPC Timestamp 
-# setting SOPC_TIMESTAMP is 1686333517
-SOPC_SYSID_FLAG += --timestamp=1686333517
-ELF_PATCH_FLAG  += --timestamp 1686333517
+# setting SOPC_TIMESTAMP is 1686592751
+SOPC_SYSID_FLAG += --timestamp=1686592751
+ELF_PATCH_FLAG  += --timestamp 1686592751
 
 # Enable JTAG UART driver to recover when host is inactive causing buffer to 
 # full without returning error. Printf will not fail with this recovery. none 
