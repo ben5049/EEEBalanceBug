faeb168ad4d20108621fc1f0bed67df573918458
Ben Smith <bens15351@gmail.com>
2023-06-15 15:35:21 +0100

    fpga config files

--- Microcontroller_code/main/TaskMovement.cpp
+++ Microcontroller_code/main/TaskMovement.cpp
@@ -85,10 +85,6 @@ TaskHandle_t taskMovementHandle = nullptr;
 hw_timer_t* motorTimerL = NULL;
 hw_timer_t* motorTimerR = NULL;
 
-
-
-
-
 //-------------------------------- Functions --------------------------------------------
 
 /**/
