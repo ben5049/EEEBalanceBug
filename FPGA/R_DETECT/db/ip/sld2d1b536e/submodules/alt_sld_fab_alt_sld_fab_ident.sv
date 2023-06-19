4ccba4162224f124cf0a6e48c610f2e8d73bd828
hj220 <wkdgks37@gmail.com>
2023-06-19 18:37:19 +0100

    plz

--- FPGA/R_DETECT/db/ip/sld2d1b536e/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ FPGA/R_DETECT/db/ip/sld2d1b536e/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 491065a9cb73fd8b7b78
+//   DESIGN_HASH   : 8da13a350239c8609176
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h491065a9cb73fd8b7b78 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h8da13a350239c8609176 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
