1ad07ea931b14c10d267e856f7051b226e29252a
hj220 <wkdgks37@gmail.com>
2023-06-19 18:20:53 +0100

    pof

--- FPGA/R_DETECT/db/ip/Qsys/submodules/Qsys_sysid_qsys.v
+++ FPGA/R_DETECT/db/ip/Qsys/submodules/Qsys_sysid_qsys.v
@@ -49,7 +49,7 @@ module Qsys_sysid_qsys (
 
   wire    [ 31: 0] readdata;
   //control_slave, which is an e_avalon_slave
-  assign readdata = address ? 1687192629 : 0;
+  assign readdata = address ? 1687195023 : 0;
 
 endmodule
 
