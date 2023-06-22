3a9ae7b80ddc3c233cd42eb1f4f51ed42acd6eb5
aranyagupta <ag1421@ic.ac.uk>
2023-06-22 20:00:00 +0100

    fix

--- Application/server/app.py
+++ Application/server/app.py
@@ -113,11 +113,11 @@ def rover():
     #     user = int(input("Next command: "))
 
     # using command queue 
-    # if (len(commandQueue))!=0:
-    #     r.actions = []
-    #     resp = [commandQueue.pop(0)]
-    # else:
-    #     resp = []
+    if (len(commandQueue))!=0:
+        r.actions = []
+        resp = [commandQueue.pop(0)]
+    else:
+        resp = []
     
     resp = {"next_actions" : resp, "clear_queue":r.estop}
     # if rover is about to spin, set flags to turn on beacons
